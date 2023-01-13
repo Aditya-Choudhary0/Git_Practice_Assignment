83eb5f25dbf8e73606c106cfa2085dc25674a967
Aditya <adityanerves@gmail.com>
2023-01-13 21:19:52 +0530

    Problem solved

---
+++ palindrome.js
@@ -0,0 +1,14 @@
+let str="aditya";
+let string="";
+  for(let i=N-1;i<=0;i++)
+  {
+      string+=str[i];
+  }
+  if(str==string)
+  {
+      console.log("Yes")
+  }
+  else
+  {
+      console.log("No")
+  }
---
+++ prime.js
@@ -0,0 +1,14 @@
+let N=15;
+let factors=0;
+for (let i=1;i<=n;i++){
+
+if(N%i==0){
+factors++
+}
+
+}
+if (factors==0){
+console.log("prime")
+}else{
+console.log("not prime")
+}
